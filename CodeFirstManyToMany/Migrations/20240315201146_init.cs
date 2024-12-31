c98be3408e92503389bca790b54d04f4cfa0faeb
Vitaliy <vitaliypolyanskiy@gmail.com>
2024-12-31 22:32:45 +0200

    Add project files.

---
+++ CodeFirstManyToMany/Migrations/20240315201146_init.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirstManyToMany.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Continents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Continents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContinentLanguage",
+                columns: table => new
+                {
+                    ContinentsId = table.Column<int>(type: "int", nullable: false),
+                    LanguagesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContinentLanguage", x => new { x.ContinentsId, x.LanguagesId });
+                    table.ForeignKey(
+                        name: "FK_ContinentLanguage_Continents_ContinentsId",
+                        column: x => x.ContinentsId,
+                        principalTable: "Continents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ContinentLanguage_Languages_LanguagesId",
+                        column: x => x.LanguagesId,
+                        principalTable: "Languages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContinentLanguage_LanguagesId",
+                table: "ContinentLanguage",
+                column: "LanguagesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContinentLanguage");
+
+            migrationBuilder.DropTable(
+                name: "Continents");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+        }
+    }
+}
